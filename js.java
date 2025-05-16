4ec286905a85361b2cb365da51464370fa485966
kinyalinah <kinyalinah6@gmail.com>
2025-05-16 14:50:33 +0300

    updated javascript
    added ai api to my java

--- js.java
+++ js.java
@@ -1,26 +1,28 @@
 
 function generateSadPoem(event){
     event.preventDefault();
-    let poemElement = document.getElementById("sad-poem-result");
-    var app = document.getElementById('sad-poem-result');
+    let context="please generate a sad poem following the prompt. use basic HTMl. Let the first 3 words be the topic of the poem inside a <strong>. Let poem be so sad. let Use <br/> after every comma or fullstop. The poem should be in English and short";
+let apiKey="6fe8tf4ae3fc290f9f3ff43213b0b7od";
+let input= document.getElementById("sad-poem-search");
+prompt=input.value;
 
-var typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
-});
-
-typewriter
-  .pauseFor(2500)
-  .typeString('<strong>Sad Poem</strong> <br/> I am Happy')
-  .pauseFor(300)
-  .deleteChars(10)
-  .typeString('I am sad')
-  .pauseFor(1000)
-  .typeString('I am sad')
-  .pauseFor(1000000)
-  .start();
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+axios.get(apiUrl).then(generatePoem);
+    
   
 
 }
 let form = document.getElementById("sad-poem-form");
 form.addEventListener("submit", generateSadPoem);
+
+function generatePoem(response){
+  
+  let poemElement = document.getElementById("sad-poem-result");
+  new Typewriter('#sad-poem-result', {
+  strings: response.data.answer,
+  autoStart: true,
+});
+    
+  
+}
+  
